fec6cbb9933722bf33954ee64dcc2d373e02ce42
Viet An <luongvietan.231123@gmail.com>
2025-01-25 23:07:59 +0700

    Frontend : Add Hero page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navibar from "./components/Navibar";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navibar />
+      <Hero />
       <Footer />
     </div>
   );
