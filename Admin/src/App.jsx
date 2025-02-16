148d3f0cbea7449bc2dbd724dbb9f58fa6890d57
Viet An <luongvietan.231123@gmail.com>
2025-02-16 10:44:43 +0700

    create Admin folder

---
+++ Admin/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">Home</div>;
+};
+
+export default App;
